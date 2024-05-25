a0af5251da9513e54ebe78ee4546f9cf044a9fce
alemr214 <ale.mr0214@gmail.com>
2024-05-25 15:57:42 -0600

    Cambiando la sintaxis

--- src/main/java/itver/com/Main.java
+++ src/main/java/itver/com/Main.java
@@ -1,7 +1,16 @@
 package itver.com;
 
+
+import java.io.File;
+
 public class Main {
+    private static File file;
     public static void main(String[] args) {
+        if(file == null){
+            System.out.println("File not found");
+            System.exit(-1);
+        }
+
         System.out.println("Hello world!");
     }
 }
