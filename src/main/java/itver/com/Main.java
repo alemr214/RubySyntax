1a39a49714ec9b99013da1754130e37acd20d626
alemr214 <ale.mr0214@gmail.com>
2024-05-26 00:33:02 -0600

    Cambiando la estructura del main

--- src/main/java/itver/com/Main.java
+++ src/main/java/itver/com/Main.java
@@ -1,16 +1,46 @@
 package itver.com;
 
+import Analizador.*;
 
+import javax.swing.*;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 
 public class Main {
     private static File file;
     public static void main(String[] args) {
+        getFile();
         if(file == null){
             System.out.println("File not found");
             System.exit(-1);
         }
+        try{
+            Sintaxis parser = new Sintaxis(new SintaxisTokenManager(
+                    new SimpleCharStream(
+                            new FileReader(Main.file)
+                    )));
+            parser.parse();
+            System.out.println("Success");
+        }catch(ParseException e){
+            System.out.println("Error: " + e.getMessage());
+        }catch(FileNotFoundException e){
+            System.out.println("File not found: " + e.getMessage());
+        }catch (TokenMgrError e){
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 
-        System.out.println("Hello world!");
+    private static void getFile(){
+        JFileChooser search = new JFileChooser();
+        FileNameExtensionFilter filterTxt = new FileNameExtensionFilter("Text Files", "txt");
+        FileNameExtensionFilter filterRb = new FileNameExtensionFilter("Ruby files", "rb");
+        search.setFileFilter(filterTxt);
+        search.setFileFilter(filterRb);
+        int option = search.showOpenDialog(null);
+        if(option == JFileChooser.APPROVE_OPTION){
+            file = search.getSelectedFile();
+        }
     }
 }
